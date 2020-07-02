55337f4b307be3fff9d17f017d54f567a90b5799
Anton Bal’ <anton.bal@windmill.ch>
2020-07-02 14:36:28 +0300

    Initial Commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Anton Bal’ on 02.07.2020.
+//  Copyright © 2020 Anton Bal’. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
